cb2d644bc4b06da9cbe43432e1defb124ca96536
James Hall-Treworgy <James Hall-Treworgy>
2016-02-24 00:15:36 -0500

    Added Edit/Delete ROUTES to app.js and their corresponding files. Added method-override for the use of PUT and DELETE methods in HTML.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-var express    = require('express');
-var app        = express();
+var methodOverride = require('method-override');
+var bodyParser     = require('body-parser');
+var mongoose       = require('mongoose');
+var express        = require('express');
+var app            = express();
 
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -60,6 +62,40 @@ app.get('/blogs/:id', function(req, res) {
     } else {
       res.render('show', {blog: foundBlog});
     }
+  });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if(err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function(req, res) {
+  // Blog.findByIdAndUpdate(id, newData, callback)
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if(err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/'+ req.params.id);
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete('/blogs/:id', function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, req.body.blog, function(err) {
+  if(err) {
+    res.redirect('/blogs');
+  } else {
+    res.redirect('/blogs');
+  }
   });
 });
 
